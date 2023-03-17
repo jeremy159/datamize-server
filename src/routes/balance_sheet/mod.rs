80d1fbc5768341cf52d9ae3121227e92287aef60
Jeremy Dube <jeremy.dube.dev@gmail.com>
2023-03-17 14:36:08 -0400

    Fix rustfmt errors

--- src/routes/balance_sheet/mod.rs
+++ src/routes/balance_sheet/mod.rs
@@ -1,15 +1,15 @@
 mod month;
 mod months;
 mod refresh_resources;
-mod resources;
 mod resource;
+mod resources;
 mod year;
 mod years;
 
 pub use month::*;
 pub use months::*;
 pub use refresh_resources::*;
-pub use resources::*;
 pub use resource::*;
+pub use resources::*;
 pub use year::*;
 pub use years::*;
