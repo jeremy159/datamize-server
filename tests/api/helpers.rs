d59f2da6361684d432ba15c0a9cb85b7d7ee7982
Jeremy Dube <jeremy.dube.dev@gmail.com>
2023-04-26 15:31:25 -0400

    Add small delay in tests to make sure app is responsive before running tests.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -528,7 +528,8 @@ pub async fn spawn_app(db_pool: PgPool) -> TestApp {
     let application_port = application.port();
     let _ = tokio::spawn(application.run());
 
-    // tokio::time::sleep(tokio::time::Duration::from_millis(200)).await;
+    // Give time for the app to start before sending in requests. Not ideal at all...
+    tokio::time::sleep(tokio::time::Duration::from_millis(200)).await;
 
     let client = reqwest::Client::builder()
         .redirect(reqwest::redirect::Policy::none())
